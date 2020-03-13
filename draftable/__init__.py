d81ff6bb9619e3bf4e21cec04adff08a9ea05700
Samuel D. Leslie <sdl@nexiom.net>
2020-03-13 15:04:47 +1100

    Store package version in external file

--- draftable/__init__.py
+++ draftable/__init__.py
@@ -5,4 +5,9 @@ from .client import PRODUCTION_CLOUD_BASE_URL, Client
 from .endpoints.comparisons.identifier import generate_identifier
 from .endpoints.comparisons.sides import make_side
 
-__version__ = '1.1.0'
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
+
+__version__ = version('draftable-compare-api')
