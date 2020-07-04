87d0dfbc119e559af305a2f11de4cad4a226d3db
Samuel D. Leslie <sdl@nexiom.net>
2020-07-04 19:12:20 +1000

    Suppress Bandit false positive

--- draftable/endpoints/comparisons/identifier.py
+++ draftable/endpoints/comparisons/identifier.py
@@ -8,4 +8,6 @@ _RANDOM_ID_CHARSET = string.ascii_letters
 
 def generate_identifier():
     # type: () -> str
-    return "".join(random.choice(_RANDOM_ID_CHARSET) for _ in range(_RANDOM_ID_LENGTH))
+    return "".join(
+        random.choice(_RANDOM_ID_CHARSET) for _ in range(_RANDOM_ID_LENGTH)  # nosec
+    )
